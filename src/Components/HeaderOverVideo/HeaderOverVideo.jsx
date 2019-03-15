3239c49c9ac1c5accf74c964d030008fbaa7a170
unknown <no.trbl.2.u@gmail.com>
2019-03-15 14:52:31 -0400

    Touched up sizing and box overlay

--- src/Components/HeaderOverVideo/HeaderOverVideo.jsx
+++ src/Components/HeaderOverVideo/HeaderOverVideo.jsx
@@ -4,8 +4,8 @@ import './HeaderOverVideo.css'
 
 const HeaderOverVideo = () => (
   <section className="HeaderOverVideo-Container">
-    <h1 className="inner-Header">Something to Say about Groove Catcher</h1>
-    <h2 className="inner-Header">Something More to Say</h2>
+    <h1 className="inner-Header">Groove Catcher VR</h1>
+    <h2 className="inner-Header">Catch the Groove</h2>
   </section>
 )
 export default HeaderOverVideo
